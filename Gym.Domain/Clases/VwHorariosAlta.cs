5c86f904ec65acb35243bf6e26bd95fc11d2b1a8
EduardoCitelli <eduardo.citelli@hotmail.com>
2020-11-16 21:02:17 -0300

    Agregado MVC

---
+++ Gym.Domain/Clases/VwHorariosAlta.cs
@@ -0,0 +1,21 @@
+﻿namespace Gym.Domain
+{
+    using System;
+    
+    public class VwHorariosAlta
+    {
+        public int Id { get; set; }
+
+        public string Actividad { get; set; }
+
+        public DateTime Inicio { get; set; }
+
+        public DateTime Final { get; set; }
+
+        public string Profesor { get; set; }
+
+        public int CodigoSalon { get; set; }
+
+        public string Salon { get; set; }
+    }
+}
